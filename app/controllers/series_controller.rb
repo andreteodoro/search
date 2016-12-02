72279320092829dd52591e2d74c3638d815a3b84
Andre Teodoro <alteodoro@yahoo.com>
2016-12-02 20:51:05 -0200

    Adding ElasticSearch and the logic search.

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,4 +1,8 @@
 class SeriesController < ApplicationController
   def index
+    if params[:keywords].present?
+      # Uses elastickick to search for the specified keywords at the fields name and description
+      @series = Series.search params[:keywords], fields: [:name, :description]
+    end
   end
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,2 +1,3 @@
 class Series < ApplicationRecord
+  searchkick
 end
